1fc880f9a7056ef9a9e00cedf8f508360250b038
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-11-18 07:22:19 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,25 +1,55 @@
-#class calculator() {
+class Calculator:
     
-#}
-
-def sum(a, b):
-    return a + b
-def rest(a,b):
-    return a - b
-def multiplication (a,b):
-    return a * b
-def divide(a,b):
-    return a / b
-
-switch_calc = {
+    def _init_(self):
+        self.result = 0
+        
+    def sum(self, a, b):
+        self.result = a + b
+        return self.result
     
-    "case1": 
-        sum,
-    "case2": 
-        rest,
-    "case3": 
-        multiplication,
-    "case4": 
-        divide
-}
+    def rest(self, a, b):
+        self.result = a - b
+        return self.result
+    
+    def multiplication(self, a, b):
+        self.result = a * b
+        return self.result
+    
+    def divide(self, a, b):
+        self.result = a / b
+        return self.result
+    
+    #switcher for the calculator
+    def switch_calc(self, operation, a, b):
+         switch_calculator = {
+        "case1": 
+            sum,
+        "case2": 
+            rest,
+        "case3": 
+            multiplication,
+        "case4": 
+            divide
+    }
+         return switch_calculator.get(operation)(a, b)
+if __name__ == "__main__":
+    calc = Calculator()
+    operation = input("Enter the operation: ")
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    result = calc.switch_calc(operation, a, b)
+    print(f"The result: {result}")
+    
+   # print(calc.sum(2, 3))
+   # print(calc.rest(2, 3))
+   # print(calc.multiplication(2, 3))
+   # print(calc.divide(2, 3))
+#def sum(a, b):
+ #   return a + b
+#def rest(a,b):
+ #   return a - b
+#def multiplication (a,b):
+ #   return a * b
+#def divide(a,b):
+ #   return a / b
 
