b3be227560890a2f58b9e773e41f2897c089278a
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-11-17 20:54:01 -0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+#class calculator() {
+    
+#}
+
+def sum(a, b):
+    return a + b
+def rest(a,b):
+    return a - b
+def multiplication (a,b):
+    return a * b
+def divide(a,b):
+    return a / b
+
+switch_calc = {
+    
+    "case1": 
+        sum,
+    "case2": 
+        rest,
+    "case3": 
+        multiplication,
+    "case4": 
+        divide
+}
+
