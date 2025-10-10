db541c26d97b1946ea491424eee2ccb5badce43c
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2025-10-10 16:36:19 -0600

    Add files via upload

---
+++ distance.py
@@ -0,0 +1,19 @@
+#check of the distance formala
+
+import math
+variable1 = float(input("Enter x1: "))
+variable2 = float(input("Enter y1: "))
+variable3 = float(input("Enter x2: "))
+variable4 = float(input ("Enter y2:"))
+
+# distance formula 
+distance = math.sqrt((variable3 - variable1)**2 + (variable2 - variable2)**2)
+
+print("The distance is: ", distance)
+variable1 = float(input("Enter x1: "))
+variable2 = float(input("Enter y1: "))
+variable3 = float(input("Enter x2: "))
+variable4 = float(input("Enter y2: "))
+
+
+
